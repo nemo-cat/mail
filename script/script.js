8df9af99740ca7de3f6bf25cfe7bbfb6665beeb1
sori <qjxj1112@naver.com>
2024-01-26 19:28:46 +0900

    메일쓰기 스크립트 작성

--- script/script.js
+++ script/script.js
@@ -1,18 +1,19 @@
-//gnb 메뉴 리스트
+//gnb 메뉴 리스트를 담는 변수
 let gnbMenuList = document.querySelectorAll(".gnb_menu li");
 
-// 날짜
+// 날짜를 구하는 변수
 let today = new Date();
 let year = today.getFullYear();
 let month = ("0" + (today.getMonth() + 1)).slice(-2);
 let day = ("0" + today.getDate()).slice(-2);
 let todayDate = year + "-" + month + "-" + day;
 
-// 메일리스트, 메일맵
+// 메일리스트, 메일맵을 담을 변수
 let key = 0;
 let mailList = [];
 let mailMap = {};
 
+//받은 메일함과, 내게쓴 메일함에 임시 데이터 추가
 for (let i = 0; i < 10; i++) {
     addMailData({
         key: key,
@@ -22,6 +23,7 @@ for (let i = 0; i < 10; i++) {
         content: "ㅈㄱㄴ",
         date: todayDate,
         mailBox: "inMailBox",
+        status: 2,
     });
 }
 
@@ -34,43 +36,59 @@ for (let i = 0; i < 10; i++) {
         content: "ㅈㄱㄴ",
         date: todayDate,
         mailBox: "toMeMailBox",
+        status: 2,
     });
-}
-
-function changeClass(e, listIdx, clickId) {
-    /* 전체 li 요소에 클래스 active삭제, normal 추가, bgimage 검정색으로 변경 */
-    let icoBlack = "ico_mailList0";
-    let titTxt = e.firstChild.innerText;
-
-    //e.id로 해볼까?
-
-    for (let i = 0; i < gnbMenuList.length; i++) {
-        gnbMenuList[i].classList.remove("active");
-        gnbMenuList[i].classList.add("normal");
-        gnbMenuList[i].style.backgroundImage = "url(../images/" + icoBlack + (i + 1) + "_black.png)";
-    }
-
-    /* 클릭한 li 요소에 클래스 active추가 */
-    e.classList.remove("normal");
-    e.classList.add("active");
-    e.style.backgroundImage = "url(../images/" + icoBlack + listIdx + "_whtie.png)";
-    document.getElementById("content_tit").innerText = titTxt;
-
-    if (clickId == "allMailBox") {
-        addMailListAll();
-    } else if (clickId == "inMailBox") {
-        addMailListInMailBox();
-    } else if (clickId == "toMeMailBox") {
-        addMailListToMeBox();
-    } else {
-        mailListRefresh();
-    }
 }
 
 function addMailData(mailObject) {
     mailList.push(mailObject);
     mailMap[mailObject.key] = mailList;
     key += 1;
+}
+
+addMailListInMailBox();
+
+/* ========== 
+GNB 메뉴 클릭시, class변경(css) 및
+메뉴에 맞는 임시 데이터를 추가하는 함수
+ ==========  */
+function changeClass(e, listIdx, clickId) {
+    // background-image 경로를 저장하는 변수
+    let icoBlack = "ico_mailList0";
+    let titTxt = e.firstChild.innerText;
+
+    //class normal로 바꾸고, background-image를 black으로 변경
+    for (let i = 0; i < gnbMenuList.length; i++) {
+        gnbMenuList[i].classList.remove("active");
+        gnbMenuList[i].classList.add("normal");
+        gnbMenuList[i].style.backgroundImage = "url(images/" + icoBlack + (i + 1) + "_black.png)";
+    }
+
+    /*  ========== 
+    클릭한 gnb메뉴에 class active로 바꾸고,
+    bg-image를 white로 변경.
+    content-tit도 gnb메뉴 내용과 같게 변경
+     ==========  */
+
+    e.classList.remove("normal");
+    e.classList.add("active");
+    e.style.backgroundImage = "url(images/" + icoBlack + listIdx + "_whtie.png)";
+    document.getElementById("content_tit").innerText = titTxt;
+
+    //클릭한 gnb메뉴와 맞는 임시 데이터를 넣는다.
+    if (clickId == "allMailBox") {
+        addMailListAll();
+    } else if (clickId == "inMailBox") {
+        addMailListInMailBox();
+    } else if (clickId == "toMeMailBox") {
+        addMailListToMeBox();
+    } else if (clickId == "sendMailBox") {
+        addMailListSendMailBox();
+    } else if (clickId == "tempoaryMailBox") {
+        addMailListTempoaryMailBox();
+    } else {
+        mailListRefresh();
+    }
 }
 
 //전체 메일함
@@ -114,11 +132,42 @@ function addMailListToMeBox() {
     }
 }
 
-//메일함 초기화
-function mailListRefresh() {
-    document.querySelector(".mail_list").innerHTML = "";
+//보낸 메일함
+function addMailListSendMailBox() {
+    mailListRefresh();
+    for (let i = 0; i < mailList.length; i++) {
+        let mailBox = mailList[i].mailBox;
+
+        if (mailBox == "sendMailBox") {
+            let sender = mailList[i].sender;
+            let title = mailList[i].title;
+            let date = mailList[i].date;
+            addMailHtmlText(sender, title, date);
+        }
+    }
 }
 
+//임시 메일함
+function addMailListTempoaryMailBox() {
+    mailListRefresh();
+    for (let i = 0; i < mailList.length; i++) {
+        let mailBox = mailList[i].mailBox;
+
+        if (mailBox == "tempoaryMailBox") {
+            let sender = mailList[i].sender;
+            let title = mailList[i].title;
+            let date = mailList[i].date;
+            addMailHtmlText(sender, title, date);
+        }
+    }
+}
+
+//메일함 초기화
+function mailListRefresh() {
+    document.querySelector(".mail_list").innerHTML = " ";
+}
+
+//메일 리스트 li를 추가하는 함수
 function addMailHtmlText(sender, title, date) {
     let mail_list = `
     <li>
---
+++ script/writeScript.js
@@ -0,0 +1,47 @@
+//임시메일 인덱스를 담아두는 변수
+let index = key;
+
+/* ========== 
+임시메일 인덱스를 통해서 input값이 바뀔떄마다
+안의 내용 변경
+ ========== */
+//받는 사람
+function writeReceiver() {
+    let receiver = document.getElementById("sendMailReceiver").value;
+    mailList[index].receiver = receiver;
+}
+
+//메일 제목
+function writeTitle() {
+    let title = document.getElementById("sendMailTitle").value;
+    mailList[index].title = title;
+}
+
+//메일 내용
+function writeContent() {
+    let content = document.getElementById("sendMailContent").value;
+    mailList[index].content = content;
+}
+
+//전송버튼 클릭시 상태값 1(전송)&보낸메일함으로 변경
+function statusChange() {
+    mailList[index].status = 1;
+    mailList[index].mailBox = "sendMailBox";
+}
+
+/* 메일작성하는 함수 */
+function writeMail() {
+    addMailData({
+        key: key,
+        sender: "qjxj1112@naver.com", //보내는 사람은 고정
+        receiver: "",
+        title: "",
+        content: "",
+        date: todayDate,
+        mailBox: "tempoaryMailBox", //임시보관함에 저장 후 전송시 보낸메일함
+        status: 0, //전송 눌러야 1로 변경, mailBox도 보낸메일함으로 이동
+    });
+}
+
+//임시메일 하나 생성
+writeMail();
